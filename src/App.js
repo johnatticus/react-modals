0e0f6928c14a4b20906634103d471a23a32bd339
John Hysong <99369137+johnatticus@users.noreply.github.com>
2023-04-11 23:20:56 -0400

    messing around

--- src/App.js
+++ src/App.js
@@ -1,57 +1,136 @@
 import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
+import { projects } from './data';
+import './App.css';
 
-const vocData = {
-  voc1: { title: "voc1-value", href: "#", content: "voc1-content" },
-  voc2: { title: "voc2-value", href: "#", content: "voc2-content" },
-  voc3: { title: "voc3-value", href: "#", content: "voc3-content" },
-  voc4: { title: "voc4-value", href: "#", content: "voc4-content" }
-};
+// const vocData = {
+//   voc1: { title: "voc1-value", href: "#", content: "voc1-content" },
+//   voc2: { title: "voc2-value", href: "#", content: "voc2-content" },
+//   voc3: { title: "voc3-value", href: "#", content: "voc3-content" },
+//   voc4: { title: "voc4-value", href: "#", content: "voc4-content" }
+// };
 
 export default function App() {
   const [vocToShow, setVocToShow] = useState(null);
-  const showModal = (voc) => setVocToShow(voc);
+  const showModal = (item) => setVocToShow(item);
   const hideModal = () => setVocToShow(null);
   return (
     <div className="p-3">
       {vocToShow && (
-        <MyModal show={vocToShow} voc={vocToShow} onClose={hideModal} />
+        <MyModal show={vocToShow} item={vocToShow} onClose={hideModal} />
       )}
       <ul className="list-inline p-3">
-        {Object.keys(vocData).map((voc, key) => {
-          console.log(vocData[voc]);
+        {Object.keys(projects).map((item, key) => {
+          console.log(projects[item]);
           return (
             <li key={key} className="list-inline-item voc-item">
-              <Button onClick={() => showModal(vocData[voc])}>
-                <small>{vocData[voc].title}</small>
+              <Button onClick={() => showModal(projects[item])}>
+                <small>{projects[item].title}</small>
               </Button>
             </li>
           );
         })}
+        
       </ul>
     </div>
   );
 }
 
-function MyModal({ show, voc, onClose }) {
+function MyModal({ show, item, onClose }) {
   return (
     <Modal
       show={show}
       onHide={onClose}
       backdrop="static"
       keyboard={false}
-      aria-labelledby={`contained-modal-title-${voc.href}`}
+      aria-labelledby={`contained-modal-title-${item.id}`}
       centered
+      className="p"
     >
-      <Modal.Header closeButton>
-        <Modal.Title id={`contained-modal-title-${voc.href}`}>
-          {voc.title}
+      <Modal.Header 
+        closeButton
+        className="special-modal"
+      >
+        <Modal.Title id={`contained-modal-title-${item.id}`}>
+          {item.title}
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>{voc.content}</Modal.Body>
+      <Modal.Body>{item.description}</Modal.Body>
     </Modal>
   );
 }
+
+
+////////////////////////////////////////////////////////////////
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Modal from 'react-modal';
+
+// const customStyles = {
+//   content: {
+//     top: '50%',
+//     left: '50%',
+//     right: 'auto',
+//     bottom: 'auto',
+//     marginRight: '-50%',
+//     transform: 'translate(-50%, -50%)',
+//   },
+// };
+
+// // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+// // Modal.setAppElement('#yourAppElement');
+
+// function App() {
+//   let subtitle;
+//   const [modalIsOpen, setIsOpen] = React.useState(false);
+
+//   function openModal() {
+//     setIsOpen(true);
+//   }
+
+//   function afterOpenModal() {
+//     // references are now sync'd and can be accessed.
+//     subtitle.style.color = '#f00';
+//   }
+
+//   function closeModal() {
+//     setIsOpen(false);
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={openModal}>Open Modal</button>
+//       <Modal
+//         isOpen={modalIsOpen}
+//         onAfterOpen={afterOpenModal}
+//         onRequestClose={closeModal}
+//         style={customStyles}
+//         contentLabel="Example Modal"
+//       >
+//         <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+//         <button onClick={closeModal}>close</button>
+//         <div>I am a modal</div>
+//         <form>
+//           <input />
+//           <button>tab navigation</button>
+//           <button>stays</button>
+//           <button>inside</button>
+//           <button>the modal</button>
+//         </form>
+//       </Modal>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
 
 
 // import logo from './logo.svg';
---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const projects = [
+    {
+      id: "project1",
+      img: "https://raw.githubusercontent.com/johnatticus/friends-in-training/main/public/images/signUp.png",
+      link: "https://afternoon-island-74965.herokuapp.com/login",
+      title: "Friends in Training",
+      description: "Friends in Training modal. Made with js and mysql db.",
+    },
+    {
+      id: "project2",
+      img: "https://user-images.githubusercontent.com/99369137/163577743-4a735fde-5b2c-4311-bde2-5be432e952c9.png",
+      link: "https://guzzemetti.github.io/economic-eats/",
+      title: "Economic Eats",
+      description: "Economic Eats modal",
+    },
+    {
+      id: "project3",
+      img: "https://raw.githubusercontent.com/johnatticus/react-portfolio-v2/main/src/img/team-profile-generator.png",
+      link: "https://github.com/johnatticus/Team-Profile-Generator",
+      title: "Team Profile Generator",
+      description: "this is example change me later",
+    },
+    {
+      id: "project4",
+      img: "https://raw.githubusercontent.com/johnatticus/Quiz-Game/main/assets/images/quizscreenshot.png",
+      link: "https://johnatticus.github.io/Quiz-Game/",
+      title: "Gonzo Quiz",
+      description: "this is example change me later",
+    },
+    {
+      id: "project5",
+      img: "https://raw.githubusercontent.com/johnatticus/Password_Generator/main/pwgeneratorscreenshot.png",
+      link: "https://johnatticus.github.io/Password_Generator/",
+      title: "Password Generator",
+      description: "this is example change me later",
+    },
+    {
+      id: "project6",
+      img: "https://raw.githubusercontent.com/johnatticus/Social-Network-API/main/assets/images/screenshot.png",
+      link: "https://github.com/johnatticus/Social-Network-API",
+      title: "Social Network API",
+      description: "this is example change me later",
+    },
+    {
+      id: "project7",
+      img: "https://raw.githubusercontent.com/johnatticus/private-practice-v2/main/client/src/assets/screenshot.png?token=GHSAT0AAAAAABZRZ7RTE2R5Z5KQR4ILMT4OY7Y3BQQ",
+      link: "https://johnatticus.github.io/Password_Generator/",
+      title: "Private Practice",
+      description: "this is example change me later",
+    },
+    {
+      id: "project8",
+      img: "https://raw.githubusercontent.com/johnatticus/MVC-Tech-Blog/main/assets/images/screenshot.png",
+      link: "https://github.com/johnatticus/MVC-Tech-Blog",
+      title: "MVC Tech Blog",
+      description: "this is example change me later",
+    },
+  ];
